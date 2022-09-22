03dfcf10a642ce8c326921fb996a4e70c29d8708
René Rüthlein <unverbuggt@xn--rthlein-n2a.de>
2022-09-22 21:43:48 +0200

    make encrypted_something of encryptcontent plugin usable

--- w3css_theme/js/theme.js
+++ w3css_theme/js/theme.js
@@ -33,10 +33,8 @@ function myAccordion(id) {
   var x = document.getElementById(id);
   if (x.className.indexOf("w3-show") == -1) {
     x.className += " w3-show";
-    x.previousElementSibling.className += " w3-theme-l4";
   } else { 
     x.className = x.className.replace("w3-show", "");
-    x.previousElementSibling.className = x.previousElementSibling.className.replace(" w3-theme-l4", "");
   }
 }
 

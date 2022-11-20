997f7a4e1aa2773429c6f1afe5913f3f9164491c
René Rüthlein <unverbuggt@xn--rthlein-n2a.de>
2022-11-20 20:02:28 +0100

    Rename theme. Not affiliated with w3schools, so why should I use their name?

--- setup.py
+++ setup.py
@@ -4,27 +4,27 @@ VERSION = '0.1'
 
 
 setup(
-    name="mkdocs-w3css-theme",
+    name="mkdocs-risonia-theme",
     version=VERSION,
-    url='https://github.com/unverbuggt/mkdocs-w3css-theme',
+    url='https://github.com/unverbuggt/mkdocs-risonia-theme',
     license='MIT',
     python_requires='>=3.5',
     install_requires=[
         'mkdocs',
         'beautifulsoup4',
     ],
-    description='w3css theme for MkDocs',
+    description='risonia theme for MkDocs',
     author='René Rüthlein',
     author_email='unverbuggt@xn--rthlein-n2a.de',
     packages=find_packages(),
     include_package_data=True,
     entry_points={
         'mkdocs.themes': [
-            'w3csstheme = w3css_theme',
+            'risonia = risonia_theme',
         ],
         'mkdocs.plugins': [
-            'color-theme = w3css_theme.plugins.color.plugin:W3cssColorTheme',
-            'w3css-classes = w3css_theme.plugins.classes.plugin:W3cssClassesPlugin'
+            'color-theme = risonia_theme.plugins.color.plugin:W3cssColorTheme',
+            'w3css-classes = risonia_theme.plugins.classes.plugin:W3cssClassesPlugin'
         ]
     },
     zip_safe=False

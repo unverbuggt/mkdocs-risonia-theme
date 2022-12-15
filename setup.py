dc9bf8aec2ab51babfb0db71ed629570301bc8bb
René Rüthlein <unverbuggt@xn--rthlein-n2a.de>
2022-12-15 19:08:09 +0100

    fix setup.py

--- setup.py
+++ setup.py
@@ -1,19 +1,28 @@
+import os
 from setuptools import setup, find_packages
 
-VERSION = '0.1'
+VERSION = '0.1.1'
 
+def read(fname):
+    file_path = os.path.join(os.path.dirname(__file__), fname)
+    with open(file_path) as file:
+        content = file.read()
+    return content if content else 'no content read'
 
 setup(
     name="mkdocs-risonia-theme",
     version=VERSION,
     url='https://github.com/unverbuggt/mkdocs-risonia-theme',
+    description='A simple theme for MkDocs using using the w3.css framework and configurable color schemes',
+    long_description=read('README.md'),
+    long_description_content_type='text/markdown',
+    keywords='mkdocs theme python markdown',
     license='MIT',
     python_requires='>=3.5',
     install_requires=[
         'mkdocs',
         'beautifulsoup4',
     ],
-    description='risonia theme for MkDocs',
     author='René Rüthlein',
     author_email='unverbuggt@xn--rthlein-n2a.de',
     packages=find_packages(),
